1bf124e7e5222b1b2a90240dcbe92e8f82350a73
achal <achal@idea-portal.co>
2018-07-26 01:29:15 +0530

    scrape  task it-1

--- lib/tasks/fetch_data.rb
+++ lib/tasks/fetch_data.rb
@@ -3,24 +3,36 @@ require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
 require "selenium-webdriver"
+require 'pry'
 csv_text = File.read("./instagram-list-Task-.csv")
 csv = CSV.parse(csv_text)
 csv.flatten!
 csv.shift
   include Capybara::DSL
-  chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-  chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
   Capybara.configure do |config|
     config.default_driver = :selenium
     config.javascript_driver = :selenium
     config.run_server = false
     config.app_host = "https://kannada.pratilipi.com"
   end
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
-  end
-  Capybara.register_driver :headless_chrome do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-    Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
-  end
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities
+end
   @session = Capybara::Session.new(:headless_chrome)
+csv.delete_if do |ig_handle|
+  @session.visit("https://www.instagram.com/#{ig_handle}/?hl=en")
+  if @session.has_text?("Sorry, this page isn't available.")
+    true
+  end
+end
+binding.pry
