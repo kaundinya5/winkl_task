03678334625b2b82bbf28af67617741503f0a8e8
achal <achal@idea-portal.co>
2018-07-28 00:59:03 +0530

    Fixed migrations and colum names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727181737) do
+ActiveRecord::Schema.define(version: 20180727192557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180727181737) do
     t.string   "hashtags",   default: [],              array: true
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.string   "user"
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
