f4c428a085937d0aa2b6efddffd4143e8b5a555c
achal <achal@idea-portal.co>
2018-07-27 23:41:00 +0530

    Iteration 2

--- lib/tasks/fetch_data.rb
+++ lib/tasks/fetch_data.rb
@@ -1,39 +1,21 @@
 require 'csv'
-require 'capybara'
-require 'capybara/dsl'
-require 'capybara/poltergeist'
-require "selenium-webdriver"
-require 'pry'
-csv_text = File.read("lib/instagram-list-Task-.csv")
-csv = CSV.parse(csv_text)
-csv.flatten!
-csv.shift
-  include Capybara::DSL
-  Capybara.configure do |config|
-    config.default_driver = :selenium
-    config.javascript_driver = :selenium
-    config.run_server = false
-    config.app_host = "https://kannada.pratilipi.com"
+require 'insta_scrape'
+csv_text = File.read("../instagram-list-Task-.csv")
+users = CSV.parse(csv_text)
+users.flatten!
+users.shift
+users.each do |user|
+  engagement = []
+  ig_user = InstaScrape.user_info_and_posts("the_sassssy_chick",include_meta_data: true)
+  if ig_user==nil
+    next
   end
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
-  )
-
-  Capybara::Selenium::Driver.new app,
-                                 browser: :chrome,
-                                 desired_capabilities: capabilities
-end
-  @session = Capybara::Session.new(:headless_chrome)
-csv.delete_if do |ig_handle|
-  @session.visit("https://www.instagram.com/#{ig_handle}/?hl=en")
-  if @session.has_text?("Sorry, this page isn't available.")
-    true
-  else
-    binding.pry
+  ig_user.posts.each do |ig_post|
+    eng = (ig_post.likes+ig_post.comments)/ig_user.follower_count
+    engagement << eng
   end
+  avg_engagement = engagement.instance_eval { reduce(:+) / size.to_f }
+  User.create(name: ig_user.username,email: ig_user.email, followers: ig_user.follower_count,
+              following: ig_user.following_count, number_of_posts: ig_user.post_count,
+              avg_engagement: avg_engagement)
 end
