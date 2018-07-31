0368b6edc1cfba9d97601f6f4862e05ac8c3e210
kaundinya5 <kaundinya5@gmail.com>
2018-07-31 16:10:20 +0530

    Task almost done.

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -4,10 +4,34 @@ WINKL.Filters = new function() {
     
   };
   this.apply = function (){
-       var selectedFiltersJSON ={};
-      $('input[type=radio]:checked').each(function (val,index) {
-          console.log(val)
-          console.log(index)
-      })
+      $("#js-users-profiles-result").empty();
+      var selectedFiltersJSON ={};
+      $('input[type=radio]:checked').each(function () {
+          var typeOfFilter = $(this).attr('name');
+          var valueOfFilter = $(this).val();
+          selectedFiltersJSON[typeOfFilter] = valueOfFilter;
+      });
+      $.ajax({
+          url: "/filter",
+          type: "GET",
+          async: true,
+          data: {filter: selectedFiltersJSON},
+          dataType: "json"
+      }).success(function(data) {
+          console.log(data);
+          var users = data.users;
+          users.forEach(function(v,i){
+              var profileCard = '<div class="individual-profile" id="user_id_'+v.id+'">'+
+                  '<h5  class="individual-profile-name">'+ v.name+'</h5>'+
+                  '<div class="individual-profile-data">'+
+                  '<p><span>'+ v.number_of_posts +'</span> Posts</p>'+
+                  '<p><span>'+ v.following+'</span> Following</p>'+
+                  '<p><span>'+ v.followers+'</span> Followers</p>'+
+                  '</div>'+
+                  '</div>';
+              $("#js-users-profiles-result").append(profileCard)
+          });
+
+      });
   }
 };
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,78 @@ class UsersController < ApplicationController
   def index
 
   end
+  def filter
+    all_scope = User
+    if params[:filter].has_key?(:emailPresent)
+      if params[:filter][:emailPresent] == "yes"
+        all_scope = all_scope.where.not(:email=>nil)
+      else
+        all_scope = all_scope.where(:email=>nil)
+      end
+    end
+    if params[:filter].has_key?(:followers)
+      case params[:filter][:followers]
+        when "<500"
+          all_scope = all_scope.where("CAST(followers AS INT)<  ?", 500)
+        when "<1k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ? AND CAST(followers AS INT) < ?", 500, 1000)
+        when "<5k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ? AND CAST(followers AS INT) < ?", 1000, 5000)
+        when "<10k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ? AND CAST(followers AS INT) < ?", 5000, 10000)
+        when "<20k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ? AND CAST(followers AS INT) < ?", 10000, 20000)
+        when "<100k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ? AND CAST(followers AS INT) < ?", 200000, 100000)
+        when ">100k"
+          all_scope = all_scope.where("CAST(followers AS INT) >= ?", 100000)
+      end
+    end
+    if params[:filter].has_key?(:ratio)
+      case params[:filter][:ratio]
+        when "<0"
+
+        when "<1"
+
+        when "<5"
+
+        when "<10"
+
+      end
+    end
+    if params[:filter].has_key?(:posts)
+      case params[:filter][:posts]
+        when "<100"
+          all_scope = all_scope.where("CAST(number_of_posts AS INT) < ?", 100)
+        when "<1k"
+          all_scope = all_scope.where("CAST(number_of_posts AS INT) >= ? AND CAST(number_of_posts AS INT) < ?", 100, 1000)
+        when ">1k"
+          all_scope = all_scope.where("CAST(number_of_posts AS INT) >= ?", 1000)
+      end
+    end
+    if params[:filter].has_key?(:avgeng)
+      case params[:filter][:avgeng]
+        when "<1%"
+          all_scope = all_scope.where("CAST(avg_engagement AS INT) < ?", 1)
+        when "<5%"
+          all_scope = all_scope.where("CAST(avg_engagement AS INT) >= ? AND CAST(avg_engagement AS INT) < ?", 1, 5)
+        when "<10%"
+          all_scope = all_scope.where("CAST(avg_engagement AS INT) >= ? AND CAST(avg_engagement AS INT) < ?", 5, 10)
+        when ">10%"
+          all_scope = all_scope.where("CAST(avg_engagement AS INT) >= ?", 10)
+      end
+    end
+    if params[:filter].has_key?(:winkl)
+      case params[:filter][:avgeng]
+        when "yes"
+          all_scope = all_scope.where.not(:email=>nil)
+        when "no"
+          all_scope = all_scope.where(:email=>nil)
+      end
+    end
+    @users = all_scope.all
+    respond_to do |format|
+      format.json { render :json => {:status => 200 , :users => @users}}
+    end
+  end
 end
